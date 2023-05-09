3fac3422123bde1f5909d90dec474f3944d832bc
Noureldin-Madkour <131866393+Noureldin-Madkour@users.noreply.github.com>
2023-05-09 13:35:48 +0300

    Delete madkour_filling_request directory

--- madkour_filling_request/madkour_filling_request/doctype/template_table/template_table.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Noureldin and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class TemplateTable(Document):
-	pass
